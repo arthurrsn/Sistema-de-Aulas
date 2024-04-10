cc526cda06cf2eff1b4523549df844a1467ebd48
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-04-09 23:00:45 -0300

    Update __init__.py
    the part where we confirm that the teacher's name is "X" is finished.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+from os import system
+from platform import system as platform_system
+from time import sleep
+
 # Opções que o usuario pode escolher como resposta
 OPCOES = {
     'ALTERE': ['ALTERAR', 'ALTERAR O NOME', 'ALTERAR NOME', '1', '[1]', 'ALTERE'],
@@ -9,6 +13,26 @@ OPCOES = {
 YES_OPTIONS = {'SIM', 'YES'}
 NO_OPTIONS = {'NÃO', 'NAO', 'NO'}
 
+# Função responsável por identificar sistema operacional
+def which_system():
+    sistema = platform_system()
+    if sistema == "Windows":
+        return "Windows"
+    elif sistema == "Darwin":
+        return "Mac"
+    elif sistema == "Linux":
+        return "Linux"
+    else:
+        return "Sistema não identificado"
+
+# Função responsável por verificar qual o sistema e colocar  comando da biblioteca os compativel
+def clear():
+    sistema = which_system()
+    if sistema == 'Windows':
+        return system('cls')
+    elif sistema == 'Linux' or sistema == 'Mac':
+        return system('clear')
+
 # Função para saber se o usuário quer cadastrar novo profissional ou alterar alguma informação do profissional que já existe
 def where_to():
     while True:
@@ -18,13 +42,15 @@ def where_to():
         elif opcao_where_to in OPCOES['EDITAR']:
             return False
         else:
-            print('Invalido. Digite novamente.')
+            clear()
+            print('Invalido. Digite novamente.\n')
 
 # Função para saber o que o usuario quer fazer após digitar não para confirmar profissional
 def do_what():
     opcao_who = str(input('''O que você quer fazer?
-    [1] Alterar nome
-    [2] Confirmar novamente\n''')).upper().strip()
+                          
+[1] Alterar nome
+[2] Confirmar novamente\n\n--> ''')).upper().strip()
 
     if opcao_who in OPCOES['ALTERE']:
         return 'ALTERE'
@@ -36,7 +62,8 @@ def do_what():
 # Função para obter a confirmação do usuário
 def get_confirmation(name):
     while True:
-        confirmation = str(input(f'Você confirma o nome do profissional como "{name}"? (SIM/NÃO)\n')).upper().strip()
+        clear()
+        confirmation = str(input(f'Você confirma o nome do profissional como "{name}"? (SIM/NÃO)\n--> ')).upper().strip()
 
         if confirmation in YES_OPTIONS:
             return True
@@ -44,28 +71,58 @@ def get_confirmation(name):
             return False
         else:
             print("Por favor, responda com 'sim' ou 'não'.")
+            sleep(1.5)
+            input('Pressione <enter>')
+
+# Função para verificar se o nome está com mais de uma palavra
+def LastName_RIGHT(name_complete):
+    cutting = str(name_complete).split()
+    if len(cutting) <= 1:
+        return False
+    elif len(cutting) > 1:
+        return True
 
 # Função principal do programa
 def main():
     input('Pressione <enter> para iniciar.')
+    clear()
 
     EXECUTE_WHERETO = where_to()
     if EXECUTE_WHERETO == True:
         while True:
-            name = str(input('Qual o nome do profissional que deseja cadastrar?\n--> ')).title().strip()
-            
+            clear()
+            name = str(input('Qual o nome COMPLETO do profissional que deseja cadastrar?\n--> ')).title().strip()
+            verify_name = LastName_RIGHT(name)
+            name_number = len(str(name).split())
+
+            if not verify_name:
+                if name_number < 1:
+                    print('Inválido')
+                    sleep(1)
+                    continue
+                else:
+                    clear()
+                    print(f'O nome {name} não pode conter apenas uma palavra, digite o nome completo.')
+                    sleep(1.5)
+                    input('Pressione <enter>')
+                    continue
+
+
             while True:
                 confirmation = get_confirmation(name)
 
                 if confirmation:
+                    clear()
                     print(f'Profissional: {name}')
                     name = name.upper
-                    break
                 else:
                     action = do_what()
                     if action == 'ALTERE':
                         break
-
+                    elif action == 'CONFIRME':
+                        continue
+                    elif action == 'NOVAMENTE':
+                        continue 
 # Execução do programa
 if __name__ == "__main__":
     main()
